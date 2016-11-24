0ec2cf518ddd37cf33ae207749b6c2ef9493a456
maccyber <jonas.enge@gmail.com>
2016-11-24 14:41:40 +0100

    Dockerized

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  databaseURL: 'https://tfk-stats.firebaseio.com',
-  databaseEntrypoint: 'tilskudd',
-  appTitle: 'Livestatistikk tilskudd.t-fk.no'
+  databaseURL: process.env.LIVESTATS_DB_URL || 'https://tfk-stats.firebaseio.com',
+  databaseEntrypoint: process.env.LIVESTATS_ENTRYPOINT || 'tilskudd',
+  appTitle: process.env.LIVESTATS_APP_TITLE || 'Livestatistikk tilskudd.t-fk.no'
 }
